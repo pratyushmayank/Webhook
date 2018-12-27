5666c82698a12a25f404bf68c2ea78dc72e3a90d
Pratyush Mayank <erpratyushmayank@gmail.com>
2018-12-27 13:16:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/webhook',function(req,res){
     let response=" ";
     let responseObj={
                          "fulfillmentText" : response,
-                         "fulfillmentMessages":[{"text": {"text":"Loyalty Errors : 23"}}],
+                         "fulfillmentMessages":[{"text": {"text": ["Loyalty Errors : 23"]}}],
                          "source":""
     }
     console.log('Here is the response to dialogflow');
